dedece3892dcc43470d45376897fd5217fdb3aa5
GN Naane <gnaane@sts3d.co.za>
2025-09-14 16:59:14 +0200

    project init from start.spring.io

---
+++ piece-job-backend/src/main/java/com/nani/backend/piece_job_backend/PieceJobBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nani.backend.piece_job_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PieceJobBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PieceJobBackendApplication.class, args);
+	}
+
+}
---
+++ piece-job-backend/src/test/java/com/nani/backend/piece_job_backend/PieceJobBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nani.backend.piece_job_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PieceJobBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
