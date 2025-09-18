cd2c2306815f414a847d3b3b3c2446950b32baa5
GN Naane <gnaane@sts3d.co.za>
2025-09-18 21:24:08 +0200

    Basic class implementation - Do Not Rest Here

---
+++ piece-job-backend/src/main/java/com/nani/backend/piece_job_backend/model/Job.java
@@ -0,0 +1,4 @@
+package com.nani.backend.piece_job_backend.model;
+
+public class Job {
+}
---
+++ piece-job-backend/src/main/java/com/nani/backend/piece_job_backend/repository/pJUserRepo.java
@@ -0,0 +1,4 @@
+package com.nani.backend.piece_job_backend.repository;
+
+public interface pJUserRepo {
+}
