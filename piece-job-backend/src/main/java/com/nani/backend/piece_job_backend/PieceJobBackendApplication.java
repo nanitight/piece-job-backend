06deea002a371a62906f1ce41c548feff4bd1ede
GN Naane <gnaane@sts3d.co.za>
2025-11-15 19:40:51 +0200

    swagger openai 3 | swagger-ui/index.html

--- piece-job-backend/src/main/java/com/nani/backend/piece_job_backend/PieceJobBackendApplication.java
+++ piece-job-backend/src/main/java/com/nani/backend/piece_job_backend/PieceJobBackendApplication.java
@@ -1,6 +1,5 @@
 package com.nani.backend.piece_job_backend;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
